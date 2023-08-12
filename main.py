2b5511ca8a0ad3bae497a182aa84c79f26946fe6
Rune ☁️ <56228125+RuneDD@users.noreply.github.com>
2023-08-12 12:58:34 +0200

    Prevent false positives

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import time
 import hashlib
 from plyer import notification
+from bs4 import BeautifulSoup
 
 # URL to be scraped
 url = ""
@@ -21,12 +22,36 @@ def get_page_content(url):
     response = requests.get(url)
     return response.text
 
+def filter_content(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    # Remove script and style elements
+    for script in soup(["script", "style", "meta"]):
+        script.extract()
+    
+    # Get text
+    text = soup.get_text()
+    
+    # Break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    
+    # Break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    
+    # Drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    
+    return text
+
 def monitor():
     old_hash = ""
+    print(f'Waiting for {time_between_checks/60} minutes before the first check...')
+    time.sleep(time_between_checks)  # Wait for 5 minutes before the first check
     while True:
         print('Checking for changes...')
         current_content = get_page_content(url)
-        current_hash = hashlib.sha224(current_content.encode()).hexdigest()
+        filtered_content = filter_content(current_content)
+        current_hash = hashlib.sha224(filtered_content.encode()).hexdigest()
         if old_hash != "" and old_hash != current_hash:
             print("Change detected, sending notification...")
             send_notification("Webpage Change Detected", "The webpage at {} has changed.".format(url))
